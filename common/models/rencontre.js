3bc099f6ff6ffa3db673b4414eea1d48b63080ff
jadoux <jadoux@visiativ.com>
2016-11-25 16:10:06 +0100

    Rajout des cartes sur le détail d'une bilbiothèque

--- common/models/rencontre.js
+++ common/models/rencontre.js
@@ -1,3 +1,7 @@
 module.exports = function(Rencontre) {
-
+  Rencontre.beforeRemote('create', function(context, user, next) {
+    var req = context.req;
+    req.body.authorId = req.accessToken.userId;
+    next();
+  });
 };
